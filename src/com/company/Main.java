cc22bdaeafa6a17e86e3625251ee38c36ea90f50
many221 <many221@gmail.com>
2021-08-10 21:22:01 -0700

    new work

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+String val = "1";
+        System.out.println (val.toUpperCase( Locale.ROOT ));
+        System.out.println (val.toLowerCase( Locale.ROOT ));
 
 
     }
